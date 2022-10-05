e11cb4f63d41f6e1546d0c7edb7ba4d416c39c9c
JoseMiranda64 <j.miranda.c375@edu.utc.mx>
2022-10-04 22:16:02 -0500

    nuevo archivo

---
+++ Programaclase/Principal.java
@@ -0,0 +1,8 @@
+public class Principal{
+
+    public static void main(String[] args){
+        //instancia de la clase
+        Figuras objfigura = new Figuras();
+        objfigura.menu();
+    }
+}
