ab4fa8ad02acb45cc068bccdd9012a3ec5c35ad7
JoseMiranda64 <j.miranda.c375@edu.utc.mx>
2022-10-05 21:50:01 -0500

    nuevo archivo

---
+++ 07Arreglos/arreglosMatriz.java
@@ -0,0 +1,53 @@
+
+import java.until
+import java.util.Scanner;
+
+public class arreglosMatriz {
+    //vamos a realizar un arreglo unidimensional correrlo para ver que hay adentro de cada elemento
+public static void main (String args){ 
+
+
+
+
+    int arreglo  = new int 10 ;
+
+    Scanner entrada = new Scanner
+    (System.in);
+
+    for(int j = 0; j < arreglo.length; j ++){
+        System.out.println("Ingresa el valor de la posicion del arreglo ");
+        valor = entrada.nextInt();
+    }
+    //vamos a recorrerlo
+    for (int i = 0; i = < arreglo length; i ++){
+        System.out.println("La magia: " + arreglo i + valor);
+
+    } 
+Scanner entrada = new Scanner (System.in);
+int matriz  = new int 3 3 
+//es poder sumar las filas del arreglo y sacar el promedio
+
+//ingresar los valores de la matriz
+for(int i = 0; i < matriz.length; i ++){
+    //filas
+    System.out.println(i);
+    //columnas
+    System.out.println(j);
+    //vamos llenarla
+    System.out.println("Ingresa el valor de la fila: " + i + " ingresa el valor de la columna : " + j );
+    matriz i j = entrada nextInt();
+}
+int sumarfilas;
+//vamos a sumar y promediar
+for(int i = 0; i < matriz.length; i ++){
+    for(int j = 0; j < matriz.length; j++){
+        //sumar las filas
+        sumar filas += matriz i j;
+        //sumar filas = sumar filas + matriz i j
+        System.out.println(String.format(format: "%d", matriz i j))
+    }
+//formatos %d int %s
+//String %c char %f float
+System.out.println(String.format(format "/"))
+}
+}
